810f905c9470d421ac55e498c4ff5ef10ee56afb
Samueelx <gsammygitau@gmail.com>
2024-04-08 10:51:30 +0300

    configure cors.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -21,6 +21,22 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	}
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		/**Allow all origins*/
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
@@ -29,6 +45,8 @@ func (s *APIServer) Run() error {
 	userHandler := user.NewHandler(useStore)
 	userHandler.RegisterRoutes(subrouter)
 
+	handler := corsMiddleware(router)
+
 	log.Println("Listening on: ", s.addr)
-	return http.ListenAndServe(s.addr, router)
+	return http.ListenAndServe(s.addr, handler)
 }
